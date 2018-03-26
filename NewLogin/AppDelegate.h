933883221bbbe67a816b41c16d869cf8188a2fcf
Evgeny Knyazev <knyazev.evgeny@gmail.com>
2018-03-26 17:19:20 +0400

    Initial Commit

---
+++ NewLogin/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NewLogin
+//
+//  Created by Evgeny Knyazev on 26.03.2018.
+//  Copyright © 2018 home. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NewLogin/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NewLogin
+//
+//  Created by Evgeny Knyazev on 26.03.2018.
+//  Copyright © 2018 home. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
